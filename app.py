5f442316566f249d3026f1a22b390995398aa739
ramonbarrbosa <ramonlbarrbosa@gmail.com>
2023-09-10 11:40:50 -0300

    Adicionando um console log

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 print("Rodando o sistema de gerenciamento de jogos")
 print("Aplicação está se conecando ao banco de dados")
+# print("Outra pessoa fez o commit sem você ver na aula!")
+print("Enviando para o GitHub")
