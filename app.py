c1307e32a40fb2afa13a7575cbaeda5a9485cfc2
ramonbarrbosa <ramonlbarrbosa@gmail.com>
2023-09-10 15:00:13 -0300

    Voltado ao estado anterior onde o app.js foi alterado para app.py

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
 print("Rodando o sistema de gerenciamento de jogos")
 print("Aplicação está se conecando ao banco de dados")
-# print("Outra pessoa fez o commit sem você ver na aula!")
-print("Enviando para o GitHub")
